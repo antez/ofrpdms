f9e9b17aca84ef3c6d042164b1419597287cea9c
AnteZheng <antewater123@hotmail.com>
2013-02-20 11:54:26 -0800

    Second commit

---
+++ OFRPDMS/Controllers/StaffController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OFRPDMS.Controllers
+{
+    public class StaffController : Controller
+    {
+        //
+        // GET: /Staff/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return PartialView();
+        }
+        public ActionResult SignIn()
+        {
+            return PartialView();
+        }
+
+    }
+}
